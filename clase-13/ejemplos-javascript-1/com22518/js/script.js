a6a3e2d7567f86b3c7fa766ccdf685c1c067c702
lucas-rodriguez <lrodriguez.arevalosc@gmail.com>
2022-09-22 21:09:00 -0300

    add clase 14

---
+++ clase-13/ejemplos-javascript-1/com22518/js/script.js
@@ -0,0 +1,14 @@
+//console.log("Hola comisión 22518!!! :)")
+//console.log(5*7)
+/*Comentario
+en 
+bloque*/
+// document.write("<h3 style='color:red;'>Hola a todos!</h3>")
+console.log("Texto") 
+console.log(8*4) 
+console.log("Voy a cumplir",36+1,"años el 28/09")
+console.info(3)
+//console.clear()
+console.warn("Texto de alerta!")
+console.error("Texto de error")
+document.write("Texto desde JS externo!")
---
+++ clase-13/ejemplos-javascript-1/com22518/js/variables.js
@@ -0,0 +1,20 @@
+//Variables en JavaScript
+var edad
+console.log(edad) //undefined
+var edad2 = 36
+console.log(typeof edad2) //number
+edad2 = "Juan Pablo"
+console.log(typeof edad2) //string
+edad2 = true
+console.log(typeof edad2) //boolean (true / false)
+edad2 = (36 + 4) * 2
+console.log(typeof edad2) //80 number
+
+//constantes
+// const IVA = 21
+// console.log(IVA) 
+// IVA = 10.5 //error
+// console.log(IVA) 
+
+
+

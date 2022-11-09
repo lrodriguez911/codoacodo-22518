ae7cfc7f640ea1f1ca7f9a7eba117285b814542d
lucas-rodriguez <lrodriguez.arevalosc@gmail.com>
2022-11-08 21:37:22 -0300

    clase 27 python 3

---
+++ clase-27/ejemplos-python-3/cadenas.py
@@ -0,0 +1,87 @@
+print("****** CADENAS DE CARACTERES ******")
+#Escritura y concatenación
+'''string1 = "Aprendiendo cadenasde caracteres"
+string2 = 'en codo a codo'
+print(string1)
+print(string2)
+print(string2 , 'concatenando')
+print(string2 + 'concatenando')'''
+
+
+#Concatenar números y cadenas
+# edad = 30
+# altura = 1.80
+# datos = "La edad es " + str(edad)  + " años y la altura es " + str(altura)
+# print(datos)
+
+
+
+# Combinar comillas simples y dobles
+# print("Hola a todos 'como estan'")
+# print('Hola a todos "como estan"')
+
+
+
+#Replicación de cadenas
+# risa = 'ja'
+# carcajada = risa*5
+# print(risa)
+# print('-'*10)
+
+
+#Cadenas multilínea
+string = """strings 
+multilineas """
+strings2 = '''strings 
+multilineas con comillas
+ simples'''
+
+#Comparación de cadenas (ASCII y case sensitive)
+str1 = "@" #alt + 64 ascii 64
+str2 = "^" #alt + 94 ascii 94
+str3 = "@" #alt + 64 ascii 64
+print(str1 >  str2)
+print(str1 <  str2)
+print(str1 ==  str3)
+
+
+#Largo de cadenas y subíndices
+
+
+#Métodos upper / lower / capitalize / title
+
+
+#Rebanadas (slicing)
+
+# [:] Todos los elementos.
+
+# [start:] Todos los elementos desde el índice establecido(start).
+
+
+# [:end] Todos los elementos desde el índice cero hasta el índice establecido(end).
+
+
+# [start:end] Todos los elementos de un rango de índices.
+
+
+# [start:end:step] Todos los elementos de un rango de índices con saltos.
+
+
+# [::step] Todos los elementos con saltos.
+
+
+# [::step] Todos los elementos con saltos (negativo).
+
+
+#Operadores de pertenencia: in / not in
+
+
+
+
+#Iterando cadenas
+#For
+
+
+#While
+
+#Uso de min y max

b158e3a10897042f0678724b237b2a6122dcaf28
lucas-rodriguez <lrodriguez.arevalosc@gmail.com>
2022-11-13 21:40:48 -0300

    add methods of array

--- clase-27/ejemplos-python-3/cadenas-metodos.py
+++ clase-27/ejemplos-python-3/cadenas-metodos.py
@@ -34,19 +34,50 @@ print(str2.isdigit())
 print(str1.isalnum())'''
 
 #isupper / islower
-str = "Python"
+'''str = "Python"
 str1 = "python"
 str2 = "PYTHON"
 
 print(str.isupper())
 print(str2.isupper())
 print(str1.islower())
-
+print(type(str1))
+'''
 
 #center / ljust / rjust / zfill
+'''str0 = "Python"
+str1 = str0.center(14, '*')
+print("Center:",str1)
+
+str2 = str0.ljust(14, '*')
+print("Ljust:",str2)
+
+str3 = str0.rjust(14, '*')
+print("Rjust:", str3)
+
+num = 345
+str4 = str(num).zfill(10)
+print("Zfill:", str4)'''
 
 #lstrip / rstrip / strip
+
+'''str0 = "---Hello-World---"
+print(str0)
+print(str0.lstrip("-"))
+
+str1 = "***Hello-Python***"
+print(str1.rstrip("*"))
+
+str2 = "---Hello-Python---"
+print(str2.strip("-"))'''
 
 
 
 #find y rfind
+str0 = "Codo a Codo"
+
+print(str0)
+print(str0.find("Codo")) #From left
+
+print(str0.rfind("Codo")) #From right
+
